06122328de330c93437b1a4a4285ecf089da60a7
leeder.josh <leederjl@aston.ac.uk>
2021-01-27 19:31:15 +0000

    Menu added, Real time UI timer added

---
+++ Lab2/Assets/_Scripts/LaptopController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopController : MonoBehaviour
+{
+    private Text laptopTimeText;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+        laptopTimeText = canvas.GetComponentInChildren<Text>();
+        Debug.Log(laptopTimeText);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laptopTimeText.text = Time.timeSinceLevelLoad.ToString();
+    }
+}
